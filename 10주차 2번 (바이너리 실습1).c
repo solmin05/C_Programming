c81a7903b669506436a07e698d05feafa306941f
solmin05 <166412164+solmin05@users.noreply.github.com>
2024-11-29 00:56:58 +0900

    Create 10주차 2번 (바이너리 실습1).c

--- 10주차 2번 (바이너리 실습1).c
+++ 10주차 2번 (바이너리 실습1).c
@@ -9,9 +9,9 @@ int main()
         return 0;
     }
     gets(input);
-    //fputs(input, fp);
-    fwrite(input,strlen(input), 1, fp);
-    
+    //fputs(input, fp); (메모한거 아님)
+    fwrite(input,strlen(input), 1,   fp);
+    //     ^주소값  ^데이터 크기 ^개수 ^쓸 파일
     fclose(fp);
 
     return 0;
